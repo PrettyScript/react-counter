b5c876e11e8c127cb3bec27cd2de04e69ac6c5c8
PrettyScript <JessicaDelcy@gmail.com>
2020-06-03 12:43:34 -0400

    counter app is functional

--- src/Components/ButtonPad.jsx
+++ src/Components/ButtonPad.jsx
@@ -1,10 +1,49 @@
-import React from 'react'
+import React, { Component } from "react";
+import "../Counter.css";
+import { motion } from "framer-motion";
 
-export default function ButtonPad() {
+export default class ButtonPad extends Component {
+  state = {
+    count: 0
+  };
+
+  handleIncrement = () => {
+    // console.log("Increment");
+    this.setState({ count: this.state.count + 1 });
+  };
+
+  handleDecrement = () => {
+    // console.log("Decrement");
+    this.setState({ count: this.state.count - 1 });
+  };
+
+  handleReset = () => {
+    // console.log("Reset");
+    this.setState({ count: 0 });
+  };
+
+  handleDoubleIncrement = () => {
+    this.setState({ count: this.state.count + 2 });
+  };
+
+  displayCount = () => this.state.count;
+
+  render() {
     return (
-        <div>
-            <button>Up Button</button>
-            <button>Down Button</button>
+      <div className='counter-display'>
+        <label onClick={this.handleIncrement}>+</label>
+        <label onClick={this.handleDoubleIncrement}>+2</label>
+        <div className='number'>
+          <motion.div
+            animate={{ scale: 2, y: -30, opacity: 0.0 }}
+            transition={{ duration: 0.5 }}
+          >
+            <h2>{this.displayCount()}</h2>
+          </motion.div>
         </div>
-    )
+        <label onClick={this.handleDecrement}>-</label>
+        <label onClick={this.handleReset}>Reset</label>
+      </div>
+    );
+  }
 }
--- src/Components/Counter.jsx
+++ src/Components/Counter.jsx
@@ -1,15 +1,13 @@
-import React from 'react';
-import Display from './Display';
-import ButtonPad from './ButtonPad';
-
+import React from "react";
+import Display from "./Display";
+import ButtonPad from "./ButtonPad";
 
 function Counter() {
-    return (
-        <div className="counter">
-            <Display/>
-            <ButtonPad/>
-        </div>
-    )
+  return (
+    <div>
+      <ButtonPad />
+    </div>
+  );
 }
 
 export default Counter;
