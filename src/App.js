73296994e7fc9a34eb459ae432a64539c7b934a7
PrettyScript <JessicaDelcy@gmail.com>
2020-06-03 23:34:22 -0400

    Revert "refactored code, passed props from app js file to counter js file"
    This reverts commit 8cf8e8e6846beab7d0c68ac3c3ad47439121c7c4.

--- src/App.js
+++ src/App.js
@@ -1,79 +1,16 @@
-import React, { Component } from "react";
+import React from "react";
 import "./App.css";
 import Counter from "./Components/Counter";
 
-export default class App extends Component {
-  state = {
-    count: 0,
-    animate: false,
-    incrementValue: 2
-  };
-
-  handleIncrement = () => {
-    this.handleAnimationAndValueChange(1, "up");
-  };
-
-  handleDecrement = () => {
-    this.handleAnimationAndValueChange(-1, "down");
-  };
-
-  handleReset = () => {
-    this.setState({ count: 0 });
-  };
-
-  handleCustomIncrement = () => {
-    this.handleAnimationAndValueChange(this.state.incrementValue, "up");
-  };
-
-  displayCount = () => this.state.count;
-
-  handleAnimationAndValueChange = (inc, dir) => {
-    document.getElementById(
-      "number"
-    ).className = `counter-text animate-number-${dir}`;
-    setTimeout(() => {
-      this.setState({ count: this.state.count + inc });
-      document.getElementById("number").className = "counter-text";
-    }, 250);
-  };
-
-  handleCustomValues = e => {
-    this.setState({
-      incrementValue: e.target.value == "" ? 2 : parseInt(e.target.value)
-    });
-  };
-
-  render() {
-    const {
-      handleIncrement,
-      handleDecrement,
-      handleReset,
-      handleCustomValues,
-      handleCustomIncrement,
-      handleAnimationAndValueChange,
-      displayCount,
-      count,
-      animate,
-      incrementValue
-    } = this.state;
-    return (
-      <div className="App">
-        <header className="App-header">
-          <h2>Counter App</h2>
-        </header>
-        <Counter
-          count={count}
-          animate={animate}
-          incrementValue={incrementValue}
-          onDisplayCount={displayCount}
-          onIncrement={handleIncrement}
-          onDecrement={handleDecrement}
-          onReset={handleReset}
-          onCustomValues={handleCustomValues}
-          onCustomeIncrement={handleCustomIncrement}
-          onAnimation={handleAnimationAndValueChange}
-        />
-      </div>
-    );
-  }
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h2>Counter App</h2>
+      </header>
+      <Counter />
+    </div>
+  );
 }
+
+export default App;
