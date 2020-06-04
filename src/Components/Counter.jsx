ab2532e36ae86f4a61648b04b24b5c1596e33000
PrettyScript <JessicaDelcy@gmail.com>
2020-06-03 20:25:11 -0400

    added custom increment input

--- src/Components/Counter.jsx
+++ src/Components/Counter.jsx
@@ -7,7 +7,8 @@ export default class Counter extends Component {
   state = {
     count: 0,
     animate: false,
-    fade: false
+    fade: false,
+    incrementValue: 2
   };
 
   handleIncrement = () => {
@@ -22,8 +23,8 @@ export default class Counter extends Component {
     this.setState({ count: 0 });
   };
 
-  handleDoubleIncrement = () => {
-    this.handleAnimationAndValueChange(2, "up");
+  handleCustomIncrement = () => {
+    this.handleAnimationAndValueChange(this.state.incrementValue, "up");
   };
 
   displayCount = () => this.state.count;
@@ -38,27 +39,45 @@ export default class Counter extends Component {
     }, 250);
   };
 
+  handleCustomValues = e => {
+    this.setState({
+      incrementValue: e.target.value == "" ? 2 : parseInt(e.target.value)
+    });
+  };
+
   render() {
     const { fade } = this.state;
     return (
-      <div className="counter-display">
-        <label onClick={this.handleReset}>Reset</label>
-        <FontAwesomeIcon
-          className="font-awesome"
-          icon={faMinus}
-          onClick={this.handleDecrement}
-        />
-        <div className="number">
-          <h2 id="number" onChange={this.handleAnimationAndValueChange}>
-            {this.displayCount()}
-          </h2>
+      <div className="main-container">
+        <div className="counter-display">
+          <label onClick={this.handleReset}>Reset</label>
+          <FontAwesomeIcon
+            className="font-awesome"
+            icon={faMinus}
+            onClick={this.handleDecrement}
+          />
+          <div className="number">
+            <h2 id="number" onChange={this.handleAnimationAndValueChange}>
+              {this.displayCount()}
+            </h2>
+          </div>
+          <FontAwesomeIcon
+            className="font-awesome"
+            icon={faPlus}
+            onClick={this.handleIncrement}
+          />
+          <label onClick={this.handleCustomIncrement}>
+            + {this.state.incrementValue}
+          </label>
         </div>
-        <FontAwesomeIcon
-          className="font-awesome"
-          icon={faPlus}
-          onClick={this.handleIncrement}
-        />
-        <label onClick={this.handleDoubleIncrement}>+ 2</label>
+        <div>
+          <input
+            id="custom-input-value"
+            placeholder="Enter custom increment value"
+            onChange={this.handleCustomValues}
+            type="number"
+          />
+        </div>
       </div>
     );
   }
