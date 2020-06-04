8cf8e8e6846beab7d0c68ac3c3ad47439121c7c4
PrettyScript <JessicaDelcy@gmail.com>
2020-06-03 21:25:25 -0400

    refactored code, passed props from app js file to counter js file

--- src/App.js
+++ src/App.js
@@ -1,16 +1,79 @@
-import React from "react";
+import React, { Component } from "react";
 import "./App.css";
 import Counter from "./Components/Counter";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <h2>Counter App</h2>
-      </header>
-      <Counter />
-    </div>
-  );
-}
+export default class App extends Component {
+  state = {
+    count: 0,
+    animate: false,
+    incrementValue: 2
+  };
 
-export default App;
+  handleIncrement = () => {
+    this.handleAnimationAndValueChange(1, "up");
+  };
+
+  handleDecrement = () => {
+    this.handleAnimationAndValueChange(-1, "down");
+  };
+
+  handleReset = () => {
+    this.setState({ count: 0 });
+  };
+
+  handleCustomIncrement = () => {
+    this.handleAnimationAndValueChange(this.state.incrementValue, "up");
+  };
+
+  displayCount = () => this.state.count;
+
+  handleAnimationAndValueChange = (inc, dir) => {
+    document.getElementById(
+      "number"
+    ).className = `counter-text animate-number-${dir}`;
+    setTimeout(() => {
+      this.setState({ count: this.state.count + inc });
+      document.getElementById("number").className = "counter-text";
+    }, 250);
+  };
+
+  handleCustomValues = e => {
+    this.setState({
+      incrementValue: e.target.value == "" ? 2 : parseInt(e.target.value)
+    });
+  };
+
+  render() {
+    const {
+      handleIncrement,
+      handleDecrement,
+      handleReset,
+      handleCustomValues,
+      handleCustomIncrement,
+      handleAnimationAndValueChange,
+      displayCount,
+      count,
+      animate,
+      incrementValue
+    } = this.state;
+    return (
+      <div className="App">
+        <header className="App-header">
+          <h2>Counter App</h2>
+        </header>
+        <Counter
+          count={count}
+          animate={animate}
+          incrementValue={incrementValue}
+          onDisplayCount={displayCount}
+          onIncrement={handleIncrement}
+          onDecrement={handleDecrement}
+          onReset={handleReset}
+          onCustomValues={handleCustomValues}
+          onCustomeIncrement={handleCustomIncrement}
+          onAnimation={handleAnimationAndValueChange}
+        />
+      </div>
+    );
+  }
+}
--- src/Components/Counter.jsx
+++ src/Components/Counter.jsx
@@ -1,92 +1,48 @@
-import React, { Component } from "react";
+import React from "react";
 import "../Counter.css";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faPlus, faMinus } from "@fortawesome/free-solid-svg-icons";
 
-export default class Counter extends Component {
-  state = {
-    count: 0,
-    animate: false,
-    fade: false,
-    incrementValue: 2
-  };
-
-  handleIncrement = () => {
-    this.handleAnimationAndValueChange(1, "up");
-  };
-
-  handleDecrement = () => {
-    this.handleAnimationAndValueChange(-1, "down");
-  };
-
-  handleReset = () => {
-    this.setState({ count: 0 });
-  };
-
-  handleCustomIncrement = () => {
-    this.handleAnimationAndValueChange(this.state.incrementValue, "up");
-  };
-
-  displayCount = () => this.state.count;
-
-  handleAnimationAndValueChange = (inc, dir) => {
-    document.getElementById(
-      "number"
-    ).className = `counter-text animate-number-${dir}`;
-    setTimeout(() => {
-      this.setState({ count: this.state.count + inc });
-      document.getElementById("number").className = "counter-text";
-    }, 250);
-  };
-
-  handleCustomValues = e => {
-    this.setState({
-      incrementValue: e.target.value == "" ? 2 : parseInt(e.target.value)
-    });
-  };
-
-  render() {
-    const { fade } = this.state;
-    return (
-      <div className="main-container">
-        <div className="counter-display">
-          <label onClick={this.handleReset}>Reset</label>
-          <FontAwesomeIcon
-            className="font-awesome"
-            icon={faMinus}
-            onClick={this.handleDecrement}
-          />
-          <div className="number">
-            <h2 id="number" onChange={this.handleAnimationAndValueChange}>
-              {this.displayCount()}
-            </h2>
-          </div>
-          <FontAwesomeIcon
-            className="font-awesome"
-            icon={faPlus}
-            onClick={this.handleIncrement}
-          />
-          <label onClick={this.handleCustomIncrement}>
-            + {this.state.incrementValue}
-          </label>
+export default function Counter() {
+  const {
+    onDisplayCount,
+    onIncrement,
+    onDecrement,
+    onReset,
+    onCustomValues,
+    onCustomeIncrement,
+    onAnimation,
+    incrementValue
+  } = this.props;
+  return (
+    <div className="main-container">
+      <div className="counter-display">
+        <label onClick={onReset}>Reset</label>
+        <FontAwesomeIcon
+          className="font-awesome"
+          icon={faMinus}
+          onClick={onDecrement}
+        />
+        <div className="number">
+          <h2 id="number" onChange={onAnimation}>
+            {onDisplayCount}
+          </h2>
         </div>
-        <div>
-          <input
-            id="custom-input-value"
-            placeholder="Enter custom increment value"
-            onChange={this.handleCustomValues}
-            type="number"
-          />
-        </div>
+        <FontAwesomeIcon
+          className="font-awesome"
+          icon={faPlus}
+          onClick={onIncrement}
+        />
+        <label onClick={onCustomeIncrement}>+ {incrementValue}</label>
       </div>
-    );
-  }
+      <div>
+        <input
+          id="custom-input-value"
+          placeholder="Enter custom increment value"
+          onChange={onCustomValues}
+          type="number"
+        />
+      </div>
+    </div>
+  );
 }
-
-// <button
-//           onClick={() => this.setState({ fade: true })}
-//           onAnimationEnd={() => this.setState({ fade: false })}
-//           className={fade ? "fade" : ""}
-//         >
-//           Click me!
-//         </button>
